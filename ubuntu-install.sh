2bddbd684ae37c8366e2a9224fabc5e027140c53
suren <suren130@gmail.com>
2020-09-10 07:21:03 +1000

    install apps on ubuntu

---
+++ ubuntu-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~
+sudo apt-get update -y -qq 
+sudo apt-get install -y -qq vim
+sudo apt-get install -y -qq python-pip
+sudo apt-get install -y -qq libffi-dev libxslt-dev libssl-devp
+sudo apt-get install -y -qq git
+sudo apt-get install -y -qq ansible
+sudo apt-get install -y -qq junos-eznc jxmlease
+sudo apt-get install -y -qq Juniper.junos
+sudo apt-get install -y -qq net-tools

8584201db819c32a8a86fa217b617a53b66f1e90
bos1234 <suren130@gmail.com>
2020-09-13 10:09:22 +1000

    new files

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y -qq python-pip
 sudo apt-get install -y -qq libffi-dev libxslt-dev libssl-devp
 sudo apt-get install -y -qq git
 sudo apt-get install -y -qq ansible
-sudo apt-get install -y -qq junos-eznc jxmlease
-sudo apt-get install -y -qq Juniper.junos
+sudo pip install junos-eznc jxmlease
+sudo pip install ansible
+ansible-galaxy install Juniper.junos
 sudo apt-get install -y -qq net-tools
